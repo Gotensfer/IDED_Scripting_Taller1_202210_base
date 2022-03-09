e2e20470b12fb14ee06534d80049a6306a306bbc
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-03-08 23:55:57 -0500

    Update Sandbox.cs

--- Sandbox.cs
+++ Sandbox.cs
@@ -5,51 +5,9 @@ namespace TestProject1
 {
     internal class Sandbox
     {
-        enum EValueType
-        {
-            Two,
-            Three,
-            Five,
-            Seven,
-            Prime
-        }
-
         public static void Main(string[] args)
         {
-            Dictionary<int, EValueType> dic = new Dictionary<int, EValueType>();
-            dic.Add(2, EValueType.Two);
-            dic.Add(8, EValueType.Two);
-            dic.Add(6, EValueType.Two);
-            dic.Add(5, EValueType.Five);
-            dic.Add(7, EValueType.Seven);
-
-            int[] keys = new int[dic.Count];
-            dic.Keys.CopyTo(keys, 0);
-
-            EValueType[] values = new EValueType[dic.Count];
-            dic.Values.CopyTo(values, 0);
-
-            //Bubble sort
-
-            for (int i = 0; i < keys.Length; i++)
-            {                
-                for (int k = 0; k < keys.Length - 1; k++)
-                {
-                    int nextElement = keys[k + 1];
-
-                    if (keys[k] < nextElement)
-                    {
-                        keys[k + 1] = keys[k];
-                        keys[k] = nextElement;
-                    }
-                }
-            }
-
-
-            for (int i = 0; i < keys.Length; i++)
-            {
-                Console.WriteLine(keys[i]);
-            }
+            
 
         }
     }
